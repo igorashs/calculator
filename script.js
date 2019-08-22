61473179dba2ae1b59f19e4fff14ec1cde780e33
igorashs <igorashsirbu@gmail.com>
2019-08-22 15:43:46 +0300

    Remove bugs

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ const buttonsContainer = document.querySelector('.buttons-container');
 const resultInputText = document.querySelector('.result-text');
 const operationInputText = document.querySelector('.operation-text');
 
-let currentOperand = '0';
-
+let currentOperand = '';
 let currentOperation = '';
 
 buttonsContainer.addEventListener('click', (e) => {
@@ -57,75 +56,12 @@ buttonsContainer.addEventListener('click', (e) => {
   }
 });
 
-function insertDigit(digit) {
-  if (currentOperand == '0') currentOperand = digit;
-  else currentOperand += digit;
+function insertDigit(digit) {}
 
-  resultInputText.textContent = currentOperation + currentOperand;
-}
+function insertOperator(oper) {}
 
-function insertOperator(oper) {
-  let lastC = currentOperand.charAt(currentOperand.length - 1);
-  let firstC = currentOperand.charAt(0);
-  if (lastC == '.') currentOperand = currentOperand.replace('.', '');
-  if (firstC == '.') currentOperand = '0' + currentOperand;
+function clearAll() {}
 
-  if (
-    (currentOperand == '0' || currentOperand == '') &&
-    currentOperation != ''
-  ) {
-    lastC = currentOperation.charAt(currentOperation.length - 1);
-    if (lastC == '+' || lastC == '-' || lastC == '*' || lastC == '/') {
-      currentOperation = currentOperation.replace(/.$/, oper);
-    }
-  } else {
-    if (currentOperand != '') currentOperation += currentOperand + oper;
-    currentOperand = '0';
-  }
-  //if (currentOperand == '0') currentOperand = '';
-  resultInputText.textContent = currentOperation;
-}
+function insertDot() {}
 
-function clearAll() {
-  resultInputText.textContent = '0';
-  operationInputText.textContent = '0';
-  currentOperand = '0';
-  currentOperation = '';
-}
-
-function insertDot() {
-  if (!currentOperand.includes('.')) currentOperand += '.';
-  resultInputText.textContent = currentOperation + currentOperand;
-}
-
-function undoInput() {
-  if (currentOperation == '') {
-    if (currentOperand != '0') {
-      currentOperand = currentOperand.replace(/.$/, '');
-      if (currentOperand == '') currentOperand = '0';
-    }
-    resultInputText.textContent = currentOperand;
-  }
-
-  if (currentOperation != '' && currentOperand == '') {
-    currentOperation = currentOperation.replace(/.$/, ''); //remove operator
-    currentOperand = currentOperation.match(/\d+(\.\d+)?$/)[0];
-
-    currentOperation = currentOperation.replace(/\d+(\.\d+)?$/, '');
-    resultInputText.textContent = currentOperation + currentOperand;
-  }
-
-  if (currentOperation != '' && currentOperand != '') {
-    if (currentOperand == '0') {
-      currentOperand = currentOperand.replace(/.$/, '');
-    } else {
-      currentOperand = currentOperand.replace(/.$/, '');
-    }
-    resultInputText.textContent = currentOperation + currentOperand;
-    return;
-  }
-
-  console.log('current operand ' + currentOperand);
-  console.log('current operation ' + currentOperation);
-  console.log('result ' + resultInputText.textContent);
-}
+function undoInput() {}
